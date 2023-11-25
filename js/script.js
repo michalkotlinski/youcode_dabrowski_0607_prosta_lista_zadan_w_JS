38661aa7e631dff2ccd5b76bebf12af2e4f7d106
michalkotlinskisp1 <michalkotlinski@sp1.ostroda.pl>
2023-11-25 18:54:22 +0100

    new tasks added posiibility

--- js/script.js
+++ js/script.js
@@ -15,15 +15,42 @@
 
     for (const task of tasks){
         htmlString += `
-        <li>
+      <li 
+        ${task.done ? " style= \"text-decoration:line-through\"" : ""}>
           ${task.content}
-        </li>`
+        </li>
+        `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
+
+
+const addNewTask = (newTaskContent) => {
+  const newTask = {
+    content: newTaskContent,
+  };
+  tasks.push(newTask);
+  render();
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
   const init = () => {
     render();
-  };
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+};
 
   init();
 }
